8c9273e1cd546c0973a200c98c903cae74a05e63
Eduardo555 <eduardospillereanzolin@gmail.com>
2018-11-01 00:54:18 -0300

    Criado tela de login, finalizado Home,Criado Evento de redirecionamento entre as telas, bug com botao de login!

---
+++ Trab/app/src/main/java/com/example/user/deevparty/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.user.deevparty;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Pegar elementos do front end;
+        btnEntrar = (Button)findViewById(R.id.btnEntrar);
+
+        // Eventos Botoes;
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RedirecionaMain();
+            }
+        });
+
+    }
+
+    private  void RedirecionaMain(){
+        Intent it = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(it);
+    }
+}
