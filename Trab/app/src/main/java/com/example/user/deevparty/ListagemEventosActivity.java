968ff86a631e2b08bdd04bf91208a1dc828aaa87
Eduardo555 <eduardospillereanzolin@gmail.com>
2018-11-07 23:10:02 -0200

    Correções das telas

--- Trab/app/src/main/java/com/example/user/deevparty/ListagemEventosActivity.java
+++ Trab/app/src/main/java/com/example/user/deevparty/ListagemEventosActivity.java
@@ -1,11 +1,10 @@
 package com.example.user.deevparty;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,23 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.view.View;
 
-import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
-
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-
-    CarouselView carouselView;
-
-    int[] sampleImage = {R.drawable.image_01, R.drawable.image_02, R.drawable.image_03};
+public class ListagemEventosActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listagem_eventos);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -51,18 +41,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        // Tentando criar um scroll page
-        carouselView = (CarouselView) findViewById(R.id.carrouselView);
-        carouselView.setPageCount(sampleImage.length);
-        carouselView.setImageListener(imageListener);
     }
-
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImage [position]);
-        }
-    };
 
     @Override
     public void onBackPressed() {
@@ -98,7 +77,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -115,11 +94,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-
-    public void EventosCadastradosClick(View view) {
-        Intent it = new Intent(MainActivity.this, ListagemEventosActivity.class);
-        startActivity(it);
     }
 }
